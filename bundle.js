45e417e1850ac25a085192837ace205e29bdd290
Bruno Felipe <brunofelipehp8@gmail.com>
2019-10-30 18:09:59 -0300

    Exercicio 05

--- bundle.js
+++ bundle.js
@@ -1,17 +1,67 @@
 "use strict";
 
-// Exercicio 4.2
-function mostraInfo(_ref) {
-  var nome = _ref.nome,
-      idade = _ref.idade;
-  return "".concat(nome, " tem ").concat(idade, " anos.");
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(source, true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(source).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+// Exercicio 5.2
+var usuario = {
+  nome: 'Diego',
+  idade: 23,
+  endereco: {
+    cidade: 'Rio do Sul',
+    uf: 'SC',
+    pais: 'Brasil'
+  }
+};
+
+var usuario2 = _objectSpread({}, usuario, {
+  nome: 'Gabriel'
+});
+
+var usuario3 = _objectSpread({}, usuario, {
+  endereco: {
+    cidade: 'Lontras'
+  }
+});
+
+console.log(usuario2);
+console.log(usuario3);
+/*
+
+// Exercicio 5.1
+
+function soma(...params) {
+    
+   return params.reduce((index, proximo) => index + proximo);
+   
 }
 
-console.log(mostraInfo({
-  nome: 'Diego',
-  idade: 23
-}));
+console.log(soma(1, 2, 3, 4, 5, 6));
+console.log(soma(1, 2)); 
+
 /*
+const arr = [1, 2, 3, 4, 5, 6, 7];
+
+const [x, ...y] = arr;
+
+console.log(x);
+console.log(y);
+
+
+
+/*
+// Exercicio 4.2
+function mostraInfo({nome, idade}) {
+    return `${nome} tem ${idade} anos.`;
+   }
+ console.log(mostraInfo({ nome: 'Diego', idade: 23 }))
+
+
+
+
 // exercicio 4.1
 const empresa = {
     nome: 'Rocketseat',
--- main.js
+++ main.js
@@ -1,5 +1,45 @@
 
 
+
+// Exercicio 5.2
+const usuario = {
+    nome: 'Diego',
+    idade: 23,
+    endereco: {
+    cidade: 'Rio do Sul',
+    uf: 'SC',
+    pais: 'Brasil',
+    }
+};
+ const usuario2 =  {...usuario, nome: 'Gabriel'};
+ const usuario3 =  {...usuario, endereco: {cidade: 'Lontras'}}
+
+ console.log(usuario2);
+ console.log(usuario3);
+/*
+
+// Exercicio 5.1
+
+function soma(...params) {
+    
+   return params.reduce((index, proximo) => index + proximo);
+   
+}
+
+console.log(soma(1, 2, 3, 4, 5, 6));
+console.log(soma(1, 2)); 
+
+/*
+const arr = [1, 2, 3, 4, 5, 6, 7];
+
+const [x, ...y] = arr;
+
+console.log(x);
+console.log(y);
+
+
+
+/*
 // Exercicio 4.2
 function mostraInfo({nome, idade}) {
     return `${nome} tem ${idade} anos.`;
@@ -8,7 +48,7 @@ function mostraInfo({nome, idade}) {
 
 
 
-/*
+
 // exercicio 4.1
 const empresa = {
     nome: 'Rocketseat',
