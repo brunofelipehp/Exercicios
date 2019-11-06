da14cfac84c4e25f0065cb3a5ff3d9147d4eaa52
Bruno Felipe <brunofelipehp8@gmail.com>
2019-11-06 17:03:24 -0300

    Modulo 03 - Execicio 02

--- src/main.js
+++ src/main.js
@@ -1,9 +1,30 @@
+import axios from 'axios';
+
+async function getUserFromGithub(user) {
+    try {
+
+        const response = await axios.get(`https://api.github.com/users/${user}`)
+        console.log(response.data);
+
+    } catch (error) {
+
+        console.log('Usuario não existe');
+
+    }
+
+}
+getUserFromGithub('brunofelipehp');
+getUserFromGithub('diego3g124123');
+
+
+
 // Funão delay aciona o .then após 1s
+/*
 const delay = () => new Promise(resolve => setTimeout(resolve, 3000));
 
 
 async function umPorSegundo() {
-  
+
     await delay()
     console.log('1s')
 
@@ -15,4 +36,4 @@ async function umPorSegundo() {
 
    }
    umPorSegundo();
-  
+  */
